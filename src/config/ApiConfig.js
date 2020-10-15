a372db67f390f97fa1ab9e3e96ad4258e03719de
Sergey <kudenkovsergej@gmail.com>
2020-10-15 13:02:11 +0300

    ready authentication session

--- src/config/ApiConfig.js
+++ src/config/ApiConfig.js
@@ -1,4 +1,4 @@
 // src/config/ApiConfig.js
 export default {
-  baseURL: "ENTER BASE URL OF API HERE" // something like "http://my-host-name.xyz/api"
+  baseURL: "https://VisualStudio/SPNc3f53469-7b00-4e1d-9484-1a78a293f604" // something like "http://my-host-name.xyz/api"
 }
